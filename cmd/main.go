23f463a7b532714aff67a992f3084df574ee367d
Gokul Sujan <s.gokul@leadergroup.com>
2025-10-30 16:24:12 +0530

    Initialize Asynq server and scheduler; load environment variables and set up Redis connection.

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/gokul-leadergroup/automax3.0_hermes/jobs"
 	"github.com/hibiken/asynq"
@@ -14,6 +16,21 @@ func main() {
 		log.Panicln("Error loading .env file", err)
 	}
 
+	redisAddr := os.Getenv("REDIS_ADDR")
+	srv := asynq.NewServer(
+		asynq.RedisClientOpt{Addr: redisAddr},
+		asynq.Config{Concurrency: 5},
+	)
+
 	mux := asynq.NewServeMux()
 	mux.HandleFunc(jobs.TaskSyncViewDbWithLiveDb, jobs.SyncDatabases)
+
+	go func() {
+		jobs.StartScheduler()
+	}()
+
+	fmt.Println("🚀 Asynq worker and scheduler started...")
+	if err := srv.Run(mux); err != nil {
+		log.Fatal(err)
+	}
 }
